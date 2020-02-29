55920b7316c454118be6824ee757a5c3833dcc78
imjeen <imjeen@sina.com>
2020-02-29 15:34:33 +0800

    feat: use class for code

--- index.js
+++ index.js
@@ -1,112 +1,132 @@
 import path from 'path';
 import fs from 'fs';
+import readline from 'readline';
 import UtilDate from './utils/date.js';
-import { __dirname } from './es.js'
+import { __dirname } from './es.js';
 
 
-const CONFIG = {
-    origin_file: './origin/data.txt',
-    target_dir: './dist'
-};
+class Ship {
 
-const RESULT_DATA = [];
+    target = {
+        name: '',
+        size: '',
+        path: ''
+    }
 
-(async _ => {
-    console.log('\nstart...');
+    constructor(options = {}) {
+        this.options = {
+            origin: './origin/data.txt', // the source file
+            dist: './dist', // the target directory
+            formatter: 'yyyyMMdd_hhmmsss', // file name with the date format
+            ...options
+        };
+        this.init();
+    }
 
-    // ---------------------------------
-    // read
+    async init() {
 
-    const rl = readline.createInterface({
-        input: fs.createReadStream(CONFIG.origin_file),
-        // crlfDelay: Infinity,
-    });
+        const data = await this.readline(this.options.origin);
+        this.keepLatest(5);
+        this.createFileSync(data);
 
-    let index = 0;
-    // nodejs version must be greater than or equal to 11
-    for await (const item of rl) {
-        ++index;
+        // log
+        console.table([
+            { ...this.target }
+        ]);
 
-        // 删除回车/换行/制表符~
-        const line = item.replace(/^(\n|\r|\t)|(\n|\r|\t)$/g, "").trim();
+    }
 
-        // 为空 或 第一行
-        if (line === '' || index === 1) continue;
+    async readline(file) {
+        const DATA_LIST = [];
+        const rl = readline.createInterface({
+            input: fs.createReadStream(file),
+            // crlfDelay: Infinity,
+        });
 
-        const list = line.split(/\t|\s/).filter(v => v.trim() !== '');
+        let index = 0;
+        // nodejs version must be greater than or equal to 11
+        for await (const item of rl) {
+            ++index;
 
-        // console.log('\n 👇length: %s,第%s行=>: %s\n', list.length, index, line);
+            // 删除回车/换行/制表符~
+            const line = item.replace(/^(\n|\r|\t)|(\n|\r|\t)$/g, "").trim();
 
-        if (list.length === 10 && list[3] !== '-') {
-            RESULT_DATA.push({
-                subcat: list[0],
-                api: list[3],
-                name: list[2],
-                material: [],
-            });
-            // console.table(list)
-        } else if (list.length === 9 || (list.length === 10 && list[3] === '-')) {
-            (list.length === 10 && list[3] === '-') && list.shift();
+            // 为空 或 第一行
+            if (line === '' || index === 1) continue;
 
-            let last_item = RESULT_DATA[RESULT_DATA.length - 1];
-            // console.log('last_item', last_item);
-            last_item && last_item.material.push({
-                "id": list[0],
-                "cartoonType": list[3],
-                "type": list[4],
-                "filterType": list[5],
-                "backgroundType": list[6],
-                "makeupType": list[7],
-                "use3X": list[8],
-            });
+            const list = line.split(/\t|\s/).filter(v => v.trim() !== '');
+
+            // console.log('\n 👇length: %s,第%s行=>: %s\n', list.length, index, line);
+
+            if (list.length === 10 && list[3] !== '-') {
+                DATA_LIST.push({
+                    subcat: list[0],
+                    api: list[3],
+                    name: list[2],
+                    material: [],
+                });
+                // console.table(list)
+            } else if (list.length === 9 || (list.length === 10 && list[3] === '-')) {
+                (list.length === 10 && list[3] === '-') && list.shift();
+
+                let last_item = DATA_LIST[DATA_LIST.length - 1];
+                // console.log('last_item', last_item);
+                last_item && last_item.material.push({
+                    "id": list[0],
+                    "cartoonType": list[3],
+                    "type": list[4],
+                    "filterType": list[5],
+                    "backgroundType": list[6],
+                    "makeupType": list[7],
+                    "use3X": list[8],
+                });
+
+            }
 
         }
 
+        return DATA_LIST;
+
     }
 
+    keepLatest(N) {
 
-    // ---------------------------------
-    // write
+        const dist = this.options.dist;
 
-    // 清除旧文件：读取目录
+        if (!fs.existsSync(dist)) {
+            fs.mkdirSync(dist);
+            return false;
+        }
 
+        const list = fs.readdirSync(dist).sort((a, b) => a < b ? 1 : -1); // for newer to older
 
-    const target_dir = CONFIG.target_dir;
+        // console.log('list', list);
 
-    if (!fs.existsSync(target_dir)) {
-        fs.mkdirSync(target_dir);
-    } else {
-        let target_list = fs.readdirSync(target_dir).sort((a, b) => a < b); // 创建从早到晚的数值
+        list.slice(N, list.length).forEach((name) => {
+            fs.unlinkSync(path.join(dist, name));
+        });
 
-        // console.log(target_list);
-
-        const file_count = 5;
-
-        target_list
-            .filter((v, i) => (i + 1) < (target_list.length - file_count))
-            .forEach(name => fs.unlinkSync(path.join(target_dir, name)))
     }
 
+    createFileSync(data) {
+        const buffer_data = new Uint8Array(Buffer.from(JSON.stringify(data, null, 2)));
+        const dist = this.options.dist,
+            name = new UtilDate().format(this.options.formatter);
+        fs.writeFileSync(path.join(dist, `data_${name}.json`), buffer_data, {
+            encoding: 'utf8',
+            cwd: __dirname,
+            stdio: [process.stdin, process.stdout, process.stderr]
+        });
 
-    const data = new Uint8Array(Buffer.from(JSON.stringify(RESULT_DATA, null, 2)));
+        this.target.name = name;
+        this.target.path = path.join(dist, `data_${name}.json`)
 
-    const name = new UtilDate().format('yyyyMMdd_hhmmsss');
+        const stats = fs.statSync(this.target.path);
+        this.target.size = `${Math.round(stats.size / 1024)} kb`
 
-    fs.writeFileSync(path.join(target_dir, `data_${name}.json`), data, {
-        encoding: 'utf8',
-        cwd: __dirname,
-        stdio: [process.stdin, process.stdout, process.stderr]
-    });
+    }
 
-    const target_path = path.join(target_dir, `data_${name}.json`)
+}
 
-    const stats = fs.statSync(target_path);
-    const size = `${Math.round(stats.size / 1024)} kb`
-
-    console.table([
-        { size, path: target_path }
-    ]);
-
-
-})();
+new Ship();
 
